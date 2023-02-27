417d6e4c62625d56262f56e2e2d8481e3ba564c9
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-02-27 18:30:31 -0500

    Login page

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,8 +3,12 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <div>
-      <div>Authentication Layout</div>
-      <main><Outlet /></main>
+      <div className="flex flex-col items-center justify-center bg-gray-50 py-2 px-4">
+        Authentication Layout
+        <div className="flex w-full">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,30 +3,45 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h2>Inicia sesión en tu cuenta</h2>
-      <form>
+    <div className='flex flex-col w-full justify-center items-center'>
+      <h2 className='m-3 text-center text-3xl font-extrabold text-gray-900'>Inicia sesión en tu cuenta</h2>
+      <form className='mt-8 max-w-wd'>
         <div>
-          <input type="email" placeholder='crisquesadaco@gmail.com' required />
-          <input type="password" required />
+          <input className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md text-gray-900 focus:ring-indigo-500"
+            type="email"
+            placeholder='crisquesadaco@gmail.com'
+            required
+          />
+          <input className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md text-gray-900 focus:ring-indigo-500"
+            type="password"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor='recuerdame'>
-            <input name='recuerdame' type="checkbox" />
-            Recuerdame
-          </label>
+        <div className='flex justify-between'>
+          <div className=''>
+            <label htmlFor='recuerdame'>
+              <input name='recuerdame' type="checkbox" />
+              Recuerdame
+            </label>
+          </div>
+          <div>
+            <Link to="/">
+              ¿Olvidaste tu contraseña?
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to="/">
-            ¿Olvidaste tu contraseña?
-          </Link>
-        </div>
+
         <div>
           <Link to='/admin'>
             <button type='submit'>Iniciar sesión</button>
           </Link>
         </div>
-        
+        <div>
+          O
+        </div>
+        <div>
+          <button>Continua con Google</button>
+        </div>        
       </form>
     </div>
   );
